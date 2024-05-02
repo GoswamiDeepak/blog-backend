cb6bb642197928ec0d167e1926fa0fbbd6e0d2af
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-05-02 23:29:45 +0530

    add testing

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { createUser, loginUser } from './user.controller.js';
 const userRouter = express.Router(); 
 
+userRouter.get("/",(req,res)=> {
+    res.send('hello')
+})
 userRouter.post('/registration', createUser);    
 userRouter.post('/login', loginUser);
 
