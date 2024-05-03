79334102a1e56304457d6318257a287888e96804
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-05-03 16:58:45 +0530

    add apierror file

---
+++ src/utils/apiError.js
@@ -0,0 +1,20 @@
+class ApiError extends Error {
+    constructor(
+        statusCode = 500,
+        message = 'Internal server Error !',
+        errors = [],
+        stack = ''
+    ) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = false;
+        this.error = errors;
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.coustructor);
+        }
+    }
+}
+
+export { ApiError };
